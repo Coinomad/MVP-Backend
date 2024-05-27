1b72bd4289fe64e0a8bfe743453f3a9673c5ff23
himarnoel <olaimarnoel>
2024-05-27 13:43:36 +0100

    createwallet

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from 'dotenv';
 import authRoutesRouter from "./routes/authRoutes.js";
 import ExpressMongoSanitize from "express-mongo-sanitize";
+import { notFoundMiddleware } from "./middleware/notFoundMiddleware.js";
 
 
 
@@ -22,9 +23,10 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
-// app.use("/v1/api/wallet",walletRoutes)
+// /v1/api/auth/
 app.use("/v1/api/auth", authRoutesRouter);
-
+// for notfound
+app.use(notFoundMiddleware);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
