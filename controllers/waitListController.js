a396a98048dbd1a0093f4916407741e43a7031a7
himarnoel <olaimarnoel>
2024-06-03 12:12:40 +0100

    made a check

--- controllers/waitListController.js
+++ controllers/waitListController.js
@@ -11,7 +11,16 @@ export const WaitListController = async (req, res) => {
         message: result.error.message,
       });
     }
-
+    // check if email is already used
+    const user = await User.findOne({
+      email: result.value.email,
+    });
+    if (user) {
+      return res.status(400).json({
+        success: false,
+        message: "Email is already in use",
+      });
+    }
     // save the user on mongodb
     const newUser = new WaitListModel(result.value);
     await newUser.save();
