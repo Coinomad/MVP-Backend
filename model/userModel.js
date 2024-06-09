af5011b0cd2d29bdab9de8e28e5e591998c531dd
himarnoel <olaimarnoel>
2024-06-09 19:17:53 +0100

    made organization name unique

--- model/userModel.js
+++ model/userModel.js
@@ -30,7 +30,7 @@ const employerSchema = new Schema(
     email: { type: String, required: true, unique: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    organizationName: { type: String,  required: true },
+    organizationName: { type: String,  unique: true, required: true },
     password: { type: String, required: true },
     active: { type: Boolean, default: false },
     resetPasswordToken: { type: String, default: null },
