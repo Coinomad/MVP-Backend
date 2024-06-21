4fd48aa806eb2f7df8dca580988230c30cbe6e0f
himarnoel <olaimarnoel>
2024-06-21 10:07:27 +0100

    added middleware to protect the routes using jwt

--- routes/walletsRoutes.js
+++ routes/walletsRoutes.js
@@ -1,12 +1,11 @@
-import express from "express"
-import { sendPolygonToEmployees } from "../controllers/walletControllers/polygonControllers"
-import { sendBitcoinToEmployees } from "../controllers/walletControllers/bitcoinControllers"
+import express from "express";
+import { sendPolygonToEmployees } from "../controllers/walletControllers/polygonControllers";
+import { sendBitcoinToEmployees } from "../controllers/walletControllers/bitcoinControllers";
+import { authMiddleware } from "../middleware/ProtectRoutes";
 
-const walletRouter = express.Router()
+const walletRouter = express.Router();
 
+walletRouter.post("/send/bitcoin/", authMiddleware, sendBitcoinToEmployees);
+walletRouter.post("/send/polygon/", authMiddleware, sendPolygonToEmployees);
 
-
-walletRouter.post("/send/bitcoin/", sendBitcoinToEmployees)
-walletRouter.post("/send/polygon/", sendPolygonToEmployees)
-
-export default walletRouter
+export default walletRouter;
