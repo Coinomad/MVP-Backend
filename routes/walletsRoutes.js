8f8cbca9c228dda5fbeab2f1bfdfe7c0d20d6816
himarnoel <olaimarnoel>
2024-06-27 11:16:47 +0100

    adjusted the code

--- routes/walletsRoutes.js
+++ routes/walletsRoutes.js
@@ -1,15 +1,40 @@
 import express from "express";
 
-
 import { authMiddleware } from "../middleware/ProtectRoutes.js";
-import { sendBitcoinToAnyone, sendBitcoinToEmployee } from "../controllers/walletControllers/bitcoinControllers.js";
-import { sendPolygonToAnyone, sendPolygonToEmployee } from "../controllers/walletControllers/polygonControllers.js";
- 
+import {
+  sendBitcoinToAnyone,
+  sendBitcoinToEmployee,
+} from "../controllers/walletControllers/bitcoinControllers.js";
+import {
+  sendPolygonToAnyone,
+  sendPolygonToEmployee,
+} from "../controllers/walletControllers/polygonControllers.js";
+import { getTransactions } from "../controllers/transactionsController.js";
+
 const walletRouter = express.Router();
 
-walletRouter.post("/send-to-employee/bitcoin/", authMiddleware, sendBitcoinToEmployee);
-walletRouter.post("/send-to-employee/polygon/", authMiddleware, sendPolygonToEmployee);
+walletRouter.post(
+  "/send-to-employee/bitcoin/",
+  authMiddleware,
+  sendBitcoinToEmployee
+);
+walletRouter.post(
+  "/send-to-employee/polygon/",
+  authMiddleware,
+  sendPolygonToEmployee
+);
 
-walletRouter.post("/send-to-anyone/polygon/", authMiddleware, sendPolygonToAnyone);
-walletRouter.post("/send-to-anyone/bitcoin/", authMiddleware, sendBitcoinToAnyone);
+walletRouter.post(
+  "/send-to-anyone/polygon/",
+  authMiddleware,
+  sendPolygonToAnyone
+);
+walletRouter.post(
+  "/send-to-anyone/bitcoin/",
+  authMiddleware,
+  sendBitcoinToAnyone
+);
+
+walletRouter.post("/transactions", authMiddleware, getTransactions);
+
 export default walletRouter;
