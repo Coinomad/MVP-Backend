d0b15679abecd2fcd0518aa45c8d1674e8f696e4
himarnoel <olaimarnoel>
2024-07-03 13:17:50 +0100

    done with transations

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -1,34 +1,33 @@
 import { Employer } from "../model/userModel.js";
 
-
 export const getTransactions = async (req, res) => {
   const employerId = req.user.id;
 
   try {
     const employer = await Employer.findById(employerId);
     if (!employer) {
-      return res.status(404).json({ message: "Employer not found" });
+      return res
+        .status(404)
+        .json({ success: false, message: "Employer not found" });
     }
 
-    const transactions = await employer.populate("transactions");
+    const fetchedTransactions = await employer.populate("transactions");
 
     res.status(200).json({
       success: true,
       message: "Transaction successful",
-      data: {
-        transactions: transactions.transactions.map((transaction, index) => ({
-          senderWalletAddress: transaction.senderWalletAddress,
-          receiverWalletAddress: transaction.receiverWalletAddress,
-          amount: transaction.amount,
-          asset: transaction.asset,
-          type: transaction.direction,
-          datetime: transaction.transactionId,
-          status: transaction.status,
-          timestamp: transaction.datetime,
-          receiverName: transaction.receiverName,
-          hash: transaction.transactionId,
-        })),
-      },
+      data: fetchedTransactions.transactions.map((transaction, index) => ({
+        senderWalletAddress: transaction.senderWalletAddress,
+        receiverWalletAddress: transaction.receiverWalletAddress,
+        amount: transaction.amount,
+        asset: transaction.walletType,
+        type: transaction.direction,
+        datetime: transaction.transactionId,
+        status: transaction.status,
+        timestamp: transaction.datetime,
+        receiverName: transaction.receiverName,
+        hash: transaction.transactionId,
+      })),
     });
   } catch (error) {
     console.log(error);
