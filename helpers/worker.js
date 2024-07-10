86e9aedf333f5acc1a1f4e9653b69d295fc5e85b
himarnoel <olaimarnoel>
2024-07-10 17:33:49 +0100

    wrote the worker

---
+++ helpers/worker.js
@@ -0,0 +1,28 @@
+// const Queue = require('bull');
+
+import { scheduleBitcoinEmployeeTranscation } from "../controllers/walletControllers/bitcoinControllers.js";
+
+
+
+// Function to process a scheduled payment
+async function processScheduledPayment(job) {
+  const { userId, value } = job.data;
+  // Simulate request and response objects
+  const req = {
+    user: { id: userId },
+    body: value,
+  };
+  const res = {
+    status: (statusCode) => ({
+      json: (data) => console.log(`Response: ${JSON.stringify(data)}`),
+    }),
+  };
+
+  // Call the sendBitcoinToEmployee function
+  await scheduleBitcoinEmployeeTranscation(req, res);
+}
+
+// Add the processor to the queue
+scheduledPaymentQueue.process(processScheduledPayment);
+
+console.log('Worker is running and waiting for jobs...');
